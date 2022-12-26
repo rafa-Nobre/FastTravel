4b8ed7222e4cba9a1dd28e9c548680df6141a4c3
rafa-Nobre <denys.nobre@outlook.com>
2022-12-26 12:00:08 -0300

    Map carousel enhancement

--- src/screens/SearchResultsMap/SearchResultsMap.js
+++ src/screens/SearchResultsMap/SearchResultsMap.js
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-import {View, Text} from 'react-native';
+import {useEffect, useRef, useState} from 'react';
+import {View, FlatList, useWindowDimensions} from 'react-native';
 import MapView from 'react-native-maps';
 import CustomMarker from '../../components/CustomMarker/CustomMarker';
 import PostCarouselItem from '../../components/PostCarouselItem/PostCarouselItem';
@@ -9,9 +9,40 @@ import dataSample from '../../../assets/data/feed';
 const SearchResultsMap = () => {
   const [selectedPlaceId, setSelectedPlaceId] = useState(null);
 
+  const width = useWindowDimensions().width;
+
+  const flatlist = useRef();
+  const viewConfig = useRef({itemVisiblePercentThreshold: 70});
+  const onViewChanged = useRef(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      const selectedPlace = viewableItems[0].item;
+      setSelectedPlaceId(selectedPlace.id);
+    }
+  });
+
+  const map = useRef();
+
+  useEffect(() => {
+    if (!selectedPlaceId || !flatlist) {
+      return;
+    }
+    const index = dataSample.findIndex(place => place.id === selectedPlaceId);
+    flatlist.current.scrollToIndex({index});
+
+    const selectedPlace = dataSample[index];
+    const region = {
+      latitude: selectedPlace.coordinate.latitude,
+      longitude: selectedPlace.coordinate.longitude,
+      latitudeDelta: 0.8,
+      longitudeDelta: 0.8,
+    };
+    map.current.animateToRegion(region);
+  }, [selectedPlaceId]);
+
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
+        ref={map}
         initialRegion={{
           latitude: 28.3279822,
           longitude: -16.5124847,
@@ -29,8 +60,19 @@ const SearchResultsMap = () => {
         ))}
       </MapView>
 
-      <View style={{position: 'absolute', bottom: 40}}>
-        <PostCarouselItem postData={dataSample[0]} />
+      <View style={{position: 'absolute', bottom: 10}}>
+        <FlatList
+          ref={flatlist}
+          data={dataSample}
+          renderItem={({item}) => <PostCarouselItem postData={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={width - 60}
+          snapToAlignment={'center'}
+          decelerationRate={'fast'}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
+        />
       </View>
     </View>
   );
